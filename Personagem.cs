a0e763cabe92349114718ca0dc25dd69901b5af5
APDuarte9 <105214793+APDuarte9@users.noreply.github.com>
2022-06-20 15:13:07 +0100

    Update Personagem.cs
    Update

--- Personagem.cs
+++ Personagem.cs
@@ -10,7 +10,11 @@ public class Personagem : MonoBehaviour
     private Rigidbody rigid;
     public float Jumpforce = 500;
     public float groundDistance = 0.3f;
+    private bool jump=false;
     public LayerMask whatIsGround;
+    public AudioClip somColetavel;
+    [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject camPira;
     [SerializeField] GameObject stepRayUpper;
     [SerializeField] GameObject stepRayLower;
     [SerializeField] float stepHeight = 0.3f;
@@ -34,11 +38,19 @@ public class Personagem : MonoBehaviour
 
         if (Input.GetButtonDown("Jump"))
         {
-            rigid.AddForce(Vector3.up * Jumpforce);
+            
+            if (jump == false)
+            {
+                rigid.AddForce(Vector3.up * Jumpforce);
 
-            anim.SetTrigger("Jump");
+                anim.SetTrigger("Jump");
 
-            stepRayUpper.transform.position = new Vector3(stepRayUpper.transform.position.x, stepHeight, stepRayUpper.transform.position.z);
+                stepRayUpper.transform.position = new Vector3(stepRayUpper.transform.position.x, stepHeight, stepRayUpper.transform.position.z);
+
+    
+
+            }
+           
 
         }
 
@@ -47,6 +59,7 @@ public class Personagem : MonoBehaviour
 
             anim.SetBool("Grounded", true);
             anim.applyRootMotion = true;
+            jump = false;
 
 
 
@@ -55,6 +68,7 @@ public class Personagem : MonoBehaviour
         {
 
             anim.SetBool("Grounded", false);
+            jump = true;
         }
 
         stepClimb();
@@ -83,6 +97,41 @@ public class Personagem : MonoBehaviour
                 rigid.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
             }
         }
+
+        RaycastHit hitLowerMinus45;
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, 0.1f))
+        {
+
+            RaycastHit hitUpperMinus45;
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitUpperMinus45, 0.2f))
+            {
+                rigid.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pira"))
+        {
+            Debug.Log("GG");
+            Jogo.fogo = true;
+            mainCam.SetActive(false);
+            camPira.SetActive(true);
+            
+            //Application.Quit();
+        }
+        else if (other.gameObject.CompareTag("Coletavel"))
+        {
+            AudioSource.PlayClipAtPoint(somColetavel, transform.position);
+            Destroy(other.gameObject);
+            Debug.Log("coleta");
+            Jogo.coleta = true;
+        }
+    }
+
+   
+}
 
         RaycastHit hitLowerMinus45;
         if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, 0.1f))
